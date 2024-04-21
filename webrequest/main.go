e0ab089341f4e5cafda2b01f035b2046362d72ba
Ritik Gagde <ritikg@LT-MBP-124.local>
2024-04-21 18:26:02 +0530

    Reset sensitive file

--- webrequest/main.go
+++ webrequest/main.go
@@ -25,4 +25,7 @@ func main() {
 	content := string(databytes)
 	fmt.Println(content)
 	fmt.Println("This is done")
+
+	fmt.Printf("\033[1;35mResponse is of type: %T\033[0m\n", response)
+	fmt.Println("\033[1;32mThe response are the folowing:\033[0m", response)
 }
